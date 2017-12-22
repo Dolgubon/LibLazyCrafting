4564cd0ff0b60f336e526ab97a6af59f80b6ea70
Joseph Heinzle <canopus100@gmail.com>
2017-12-21 23:24:08 -0500

    Smithing version number updated...

--- Smithing.lua
+++ Smithing.lua
@@ -25,7 +25,7 @@
 local LibLazyCrafting = LibStub("LibLazyCrafting")
 
 local widgetType = 'smithing'
-local widgetVersion = 1.5
+local widgetVersion = 1.6
 if not LibLazyCrafting:RegisterWidget(widgetType, widgetVersion) then return  end
 
 local function dbug(...)
