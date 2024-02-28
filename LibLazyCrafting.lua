0ea5d542a0d9219a208cc5ddb4ef0e5c5cd278bf
Sharlikran <danielhmpdx16@gmail.com>
2024-02-28 08:22:07 -0800

    Version: 4.001

--- LibLazyCrafting.lua
+++ LibLazyCrafting.lua
@@ -17,7 +17,7 @@ end
 
 -- Initialize libraries
 local libLoaded
-local LIB_NAME, VERSION = "LibLazyCrafting", 4.0
+local LIB_NAME, VERSION = "LibLazyCrafting", 4.001
 local LibLazyCrafting, oldminor
 if LibStub then
 	LibLazyCrafting, oldminor = LibStub:NewLibrary(LIB_NAME, VERSION)
